8770d794c7a43f2848e55d249252eadad2f1124e
Konstantin Valiulin <Clash19856@gmail.com>
2025-03-07 10:21:09 +0300

    initial commit

---
+++ IGI/LR1/geometric_lib/circle.py
@@ -0,0 +1,12 @@
+import math
+
+
+def area(r):
+    return math.pi * r * r
+
+
+def perimeter(r):
+    return 2 * math.pi * r
+
+#Новый комментарий к файлу
+#Еще один комментарий к файлу
---
+++ IGI/LR1/geometric_lib/square.py
@@ -0,0 +1,8 @@
+
+def area(a):
+    return a * a
+
+
+def perimeter(a):
+    return 4 * a
+#Площадь квадрата вычисляется по формуле a * a
